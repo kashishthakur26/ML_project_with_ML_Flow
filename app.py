292a2b069705390d77b5bf7859fc1bae7fd4f0a3
kashishthakur26 <kashishthakur.official.0907@gmail.com>
2024-03-04 23:06:20 +0530

    final commit

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port = 8080, debug=True)
+    app.run(host="0.0.0.0",port = 8080)
